4d12b61ee04376f646a7c5a4176199cb8f5cb5be
csanchez <csanchez@fenalcoantioquia.com>
2019-04-17 14:22:32 -0500

    3 respuestas completas

--- src/main/java/com/tns/analizer/BankTransactionAnalyzerSimple.java
+++ src/main/java/com/tns/analizer/BankTransactionAnalyzerSimple.java
@@ -1,7 +1,15 @@
 package com.tns.analizer;
 
+import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.text.DateFormat;
+import java.text.FieldPosition;
+import java.text.ParsePosition;
+import java.text.SimpleDateFormat;
 import java.time.Month;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class BankTransactionAnalyzerSimple {
@@ -10,14 +18,59 @@ public class BankTransactionAnalyzerSimple {
 
     public void execute() {
         try{
-            System.out.println("Estamos listos para empezar!!");
+            //System.out.println("Estamos listos para empezar!!");            ;
+            Pago pago = new Pago();
+            BufferedReader br = new BufferedReader(new FileReader(FILE));
+            List<Pago> listaPago = new ArrayList();
+            int total = 0, totalEnero = 0;
+            String tipoNumero, fPago, linea = br.readLine();
+
+            String pattern = "dd-MM-yyyy";
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+
+            while (linea != null) {
+
+                String[] arreglo = linea.split(",");
+                Date fechaPago = simpleDateFormat.parse(arreglo[0]);
+
+                pago.setFecha_pago(fechaPago);
+                pago.setMonto(Integer.valueOf(arreglo[1]));
+                pago.setDescripcion(arreglo[2]);
+
+                listaPago.add(pago);
+                pago= new Pago();
+                linea = br.readLine();
+            }
+
+            Iterator<Pago> itp = listaPago.iterator();
+            while (itp.hasNext()) {
+                Pago p = itp.next();
+                 total += p.getMonto();
+                 fPago = simpleDateFormat.format(p.getFecha_pago());
+                 System.out.println(fPago);
+                 String[] cadenaFecha = fPago.split("-");
+
+                 if (cadenaFecha[1].equals("01")) {
+                     totalEnero += p.getMonto();
+                 }
+            }
+            if (total > 0) {
+                tipoNumero = "Positivo";
+            } else {
+                tipoNumero = "Negativo";
+            }
+
+            System.out.println("El total es: "+total+" y es "+tipoNumero+"\n");
+            System.out.println("El monto total de Enero es: "+totalEnero);
+
+            //System.out.println(pago.calcularTotal());
+
         }catch (Exception e){
             System.out.println("Error...");
             System.out.println(e.getMessage());
         }
 
     }
-
 }
 
 
--- src/main/java/com/tns/analizer/Pago.java
+++ src/main/java/com/tns/analizer/Pago.java
@@ -1,4 +1,43 @@
 package com.tns.analizer;
 
+import javafx.scene.input.DataFormat;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class Pago {
+    private Date fecha_pago;
+    private int monto;
+    private String descripcion;
+
+
+    public Date getFecha_pago() {
+        return fecha_pago;
+    }
+
+    public void setFecha_pago(Date fecha_pago) {
+        this.fecha_pago = fecha_pago;
+    }
+
+    public int getMonto() {
+        return monto;
+    }
+
+    public void setMonto(int monto) {
+        this.monto = monto;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
